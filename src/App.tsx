59e994082c126a1d5993bc722a78bb0c184166f2
aliakbar <s.a.zabihnejad@gmail.com>
2025-08-09 12:14:42 +0330

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <a className='bg-primary text-secondary' href="#">a  sdfadsf</a>
-      <button>test</button>
+    <div className=''>
+
     </div>
   )
 }
