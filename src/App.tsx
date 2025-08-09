6835596541934028622113632b892c478cdedb0c
simin <simintehraniii35@gmail.com>
2025-08-09 15:22:54 +0330

    chore:dark/light handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
 import AppRouter from './routes'
+import "./assets/Color/Color-Theme.css"
 
 function App() {
 
   return (
-    <div className=''>
+    <div className='bg-third'>
       <AppRouter />
+     <button
+  className="bg-secondary px-4 py-2 rounded text-white"
+  onClick={() => {
+    document.documentElement.classList.toggle("dark");
+  }}
+>
+  Toggle Dark Mode
+</button>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ export default {
   ],
   theme: {
     extend: {
-      
+       colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        third:'var(--third)',
+        background:'var(--background)',
+        border:'var(--border)',
+        borderPrimary:'var(--borderPrimary)',
+text:'var(--text)'
+
+      },
     },
   },
+   darkMode:"media",
   plugins: [],
 }
