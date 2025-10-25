a31ea6fce805c8998da5efd1e28fe213c5ad4b62
simin <83537206+simin-tehrani@users.noreply.github.com>
2025-10-25 13:12:42 +0330

    edit profile

--- src/Components/BreadcrumbNavigation.tsx
+++ src/Components/BreadcrumbNavigation.tsx
@@ -1,61 +1,149 @@
+// import React from "react";
+// import { Link, useLocation } from "react-router"
+// import IconChevron from "../assets/icons/trade/IconChevron";
+
+// const BREADCRUMB_LABELS: Record<string, string> = {
+
+//   trade: "معامله",
+//   buy: "خرید",
+//   sell: "فروش",
+//   options: "خدمات",
+//   Profile: "پروفایل",
+//   ticket: "تیکت‌ها",
+//   create: "ایجاد تیکت",
+//   authentication: 'احراز هویت',
+//   services: 'خدمات',
+//   transaction: 'تاریخچه تراکنش ها',
+//   "connected-devices": 'دستگاه های متصل',
+//   faq: 'سوالات متداول',
+//   "bank-cards": 'کارت های بانکی',
+//   withdraw: 'برداشت',
+//   'market-view': 'نمای بازار',
+//   "kyc-basic": "احراز هویت پایه",
+//    history:"خدمات",
+//    order:"تاریخچه خرید و فروش",
+//    toman:"تاریخچه تراکنش های تومانی",
+//    Cryptocurrency:"تاریخچه تراکنش های رمز ارز",
+//    fiat:"برداشت تومانی",
+//    crypto:"برداشت رمز ارز",
+//  "add-friend": "دعوت از دوستان",
+
+// };
+
+// const BreadcrumbNavigation = () => {
+//   const location = useLocation()
+//   const pathnames = location.pathname.split("/").filter(x => x);
+
+//   return (
+//     <nav className="w-full flex items-center gap-1 lg:gap-2 text-xs lg:text-sm font-normal" dir="rtl">
+//       <Link className="text-text2 text-black0 hover:text-blue2 hover:underline" to='/'>پی‌فا24</Link>
+//       {pathnames.map((value, index) => {
+//         const to = `/${pathnames.slice(0, index + 1).join("/")}`;
+//         const isLast = index === pathnames.length - 1;
+//         return (
+//           <React.Fragment key={to}>
+//             <span className="w-4 h-4 text-text2 text-black0"><IconChevron /></span>
+//             {isLast ?
+//               <span className="text-blue2">{BREADCRUMB_LABELS[value]}</span>
+//               :
+//               <Link className="text-black1 hover:text-blue2 hover:underline" to={to}>{BREADCRUMB_LABELS[value]}</Link>
+//             }
+//           </React.Fragment>
+//         );
+//       })}
+//     </nav>
+//   )
+// }
+
+// export default BreadcrumbNavigation
+
+
+
 import React from "react";
-import { Link, useLocation } from "react-router"
+import { Link, useLocation } from "react-router";
 import IconChevron from "../assets/icons/trade/IconChevron";
 
 const BREADCRUMB_LABELS: Record<string, string> = {
-
   trade: "معامله",
   buy: "خرید",
   sell: "فروش",
   options: "خدمات",
-  Profile: "پروفایل",
+
   ticket: "تیکت‌ها",
   create: "ایجاد تیکت",
-  authentication: 'احراز هویت',
-  services: 'خدمات',
-  transaction: 'تاریخچه تراکنش ها',
-  "connected-devices": 'دستگاه های متصل',
-  faq: 'سوالات متداول',
-  "bank-cards": 'کارت های بانکی',
-  withdraw: 'برداشت',
-  'market-view': 'نمای بازار',
+  authentication: "احراز هویت",
+  services: "خدمات",
+  transaction: "تاریخچه تراکنش ها",
+  "connected-devices": "دستگاه های متصل",
+  faq: "سوالات متداول",
+  "bank-cards": "کارت های بانکی",
+  withdraw: "برداشت",
+  "market-view": "نمای بازار",
   "kyc-basic": "احراز هویت پایه",
-   history:"خدمات",
-   order:"تاریخچه خرید و فروش",
-   toman:"تاریخچه تراکنش های تومانی",
-   Cryptocurrency:"تاریخچه تراکنش های رمز ارز",
-   fiat:"برداشت تومانی",
-   crypto:"برداشت رمز ارز",
- "add-friend": "دعوت از دوستان",
+  history: "خدمات",
+  order: "تاریخچه خرید و فروش",
+  toman: "تاریخچه تراکنش های تومانی",
+  Cryptocurrency: "تاریخچه تراکنش های رمز ارز",
+  fiat: "برداشت تومانی",
+  crypto: "برداشت رمز ارز",
+  "add-friend": "دعوت از دوستان",
 
+  profile: "پروفایل",
+"user-account": "حساب کاربری",
+  "security-settings": "تنظیمات امنیت",
+  "change-password": "تغییر رمز عبور", 
+  "multi-factor":"ورود دو مرحله ای ",
+ 
+
+
+
+  deposit: "واریز",
+  gateway: "درگاه پرداخت",
+  identifier: "شناسه",
+  card: "کارت به کارت",
+  receipt: "فیش بانکی",
+  wallet: "ولت اختصاصی",
+  txid: "TxID",
 };
 
 const BreadcrumbNavigation = () => {
-  const location = useLocation()
-  const pathnames = location.pathname.split("/").filter(x => x);
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
 
   return (
-    <nav className="w-full flex items-center gap-1 lg:gap-2 text-xs lg:text-sm font-normal" dir="rtl">
-      <Link className="text-text2 text-black0 hover:text-blue2 hover:underline" to='/'>پی‌فا24</Link>
+    <nav
+      className="w-full flex items-center gap-1 lg:gap-2 text-xs lg:text-sm font-normal"
+      dir="rtl"
+    >
+      <Link
+        className="text-text2 text-black0 hover:text-blue2 hover:underline"
+        to="/"
+      >
+        پی‌فا24
+      </Link>
       {pathnames.map((value, index) => {
         const to = `/${pathnames.slice(0, index + 1).join("/")}`;
         const isLast = index === pathnames.length - 1;
         return (
           <React.Fragment key={to}>
-            <span className="w-4 h-4 text-text2 text-black0"><IconChevron /></span>
-            {isLast ?
+            <span className="w-4 h-4 text-text2 text-black0">
+              <IconChevron />
+            </span>
+            {isLast ? (
               <span className="text-blue2">{BREADCRUMB_LABELS[value]}</span>
-              :
-              <Link className="text-black1 hover:text-blue2 hover:underline" to={to}>{BREADCRUMB_LABELS[value]}</Link>
-            }
+            ) : (
+              <Link
+                className="text-black1 hover:text-blue2 hover:underline"
+                to={to}
+              >
+                {BREADCRUMB_LABELS[value]}
+              </Link>
+            )}
           </React.Fragment>
         );
       })}
     </nav>
-  )
-}
+  );
+};
 
-export default BreadcrumbNavigation
-
-
-
+export default BreadcrumbNavigation;
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,3 +1,62 @@
+// export const ROUTES = {
+//   HOME: "/",
+//   LOGIN: "/login",
+//   REGISTER: "/register",
+//   MARKET: "/market",
+//   NOTIFICATIONS: "/notifications",
+//   FORGOT_PASSWORD: "/forgot-password",
+//   FORGOT_PASSWORD_SET_PASSWORD: "/forgot-password-set-password",
+//   AUTHENTICATION_BASIC: "/kyc-basic",
+//   AUTHENTICATION_ADVANCED: "/kyc-advanced",
+//   BANK_CARDS_CONTAINER: "/services/bank-cards",
+//   PROFILE: "/profile",
+//   USER_ACCOUNT: "./authProfile",
+//   BANK_ACCOUNT: "./authProfile",
+//   WALLET: "/wallet",
+//   CHANGE_PASSWORD: "/change-password",
+//   VERIFY_GOOGLE: "/verify/google",
+//   MULTI_FACTOR: "/multi-factor",
+//   GOOGLE_AUTH_FLOW: "/google-auth-flow",
+//   CONNECTED_DEVICES: "/services/connected-devices",
+//   FAQ: "/services/faq",
+//   BANK_CARDS: "/services/bank-cards",
+//   WITHDRAWAL_FIAT: "/withdraw/fiat",
+//   WITHDRAWAL_CRYPTO: "/withdraw/crypto",
+//   MARKET_VIEW: '/services/market-view',
+//   TRADE: {
+//     ROOT: "/trade",
+//     BUY: "/trade/buy",
+//     SELL: "/trade/sell",
+//   },
+//   TICKET: {
+//     ROOT: "/ticket",
+//     CREATE: "/ticket/create",
+//   },
+//   TRANSACTION: {
+//     ROOT: "/history",
+//     CRYPTO_HISTORY: "/history/Cryptocurrency",
+//     TOMAN_HISTORY: "/history/toman",
+//     ORDER_HISTORY: "/history/order",
+//   },
+
+  
+// ADD_FRIEND: {
+//  ROOT:"/services/add-friend", // مسیر اصلی را به زیرمجموعه خدمات ببرید
+// },
+
+
+//   DEPOSIT: "/deposit",
+//   DEPOSIT_GATEWAY: "/deposit/gateway",
+//   DEPOSIT_IDENTIFIER: "/deposit/identifier",
+//   DEPOSIT_CARD: "/deposit/card",
+//   DEPOSIT_RECEIPT: "/deposit/receipt",
+//   DEPOSIT_WALLET: "/deposit/wallet",
+//   DEPOSIT_TXID: "/deposit/txid",
+
+// };
+
+
+
 export const ROUTES = {
   HOME: "/",
   LOGIN: "/login",
@@ -44,16 +103,22 @@ ADD_FRIEND: {
  ROOT:"/services/add-friend", // مسیر اصلی را به زیرمجموعه خدمات ببرید
 },
 
+PROFILE_MEN: {
+  ROOT: "/profile",
+  USER_ACCOUNT: "/profile/user-account",
+ SECURITY_SETTINGS: "/profile/security-settings",
+    CHANGE_PASSWORD: "/profile/security-settings/change-password",
+    MULTI_FACTOR: "/profile/security-settings/multi-factor",
+  CONNECTED_DEVICES: "/profile/connected-devices",
+  BANK_CARDS: "/profile/bank-cards",
+},
 
-  DEPOSIT: "/deposit",
-  DEPOSIT_GATEWAY: "/deposit/gateway",
-  DEPOSIT_IDENTIFIER: "/deposit/identifier",
-  DEPOSIT_CARD: "/deposit/card",
-  DEPOSIT_RECEIPT: "/deposit/receipt",
-  DEPOSIT_WALLET: "/deposit/wallet",
-  DEPOSIT_TXID: "/deposit/txid",
+DEPOSIT: "/deposit",
+DEPOSIT_GATEWAY: "/deposit/closeDeal",
+DEPOSIT_IDENTIFIER: "/deposit/identifier",
+DEPOSIT_CARD: "/deposit/card",
+DEPOSIT_RECEIPT: "/deposit/receipt",
+DEPOSIT_WALLET: "/deposit/wallet",
+DEPOSIT_TXID: "/deposit/txid",
 
 };
-
-
-
