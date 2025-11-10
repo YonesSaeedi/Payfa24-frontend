497923d0be39e6daa9a42e21840044e15c5e00ee
simin <83537206+simin-tehrani@users.noreply.github.com>
2025-11-10 16:29:01 +0330

    fix banckcards

--- src/pages/BankCards/BankCardsManager.tsx
+++ src/pages/BankCards/BankCardsManager.tsx
@@ -1,6 +1,5 @@
 
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom"; 
 import { apiRequest } from "../../utils/apiClient";
 import BankCardForm from "../../components/BankCards/BankCardModal/BankCardForm";
 import BankCardList from "../../components/BankCards/BankCardList";
@@ -8,19 +7,38 @@ import BreadcrumbNavigation from "../../components/BreadcrumbNavigation";
 import HeaderLayout from "../../layouts/HeaderLayout";
 import SkeletonCard from "../../components/BankCards/SkeletonCard";
 import { AddCardApi } from "../../components/BankCards/AddCardApi";
-import { ROUTES } from "../../routes/routes";
+import BankCardsPage from "./BankcardsPage";
 
-type Card = { id: number; number: string; holder: string; bankName: string; status: "confirm" | "pending" | "rejected"; };
+
+type Card = {
+  id: number;
+  number: string;
+  holder: string;
+  bankName: string;
+  status: "confirm" | "pending" | "rejected";
+};
 
 function BankCardManager() {
   const [cards, setCards] = useState<Card[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
+ const [loading, setLoading] = useState<boolean>(true); 
+
   const [error, setError] = useState<string | null>(null);
-  const navigate = useNavigate(); 
+
+
   const fetchCards = async () => {
     try {
       setLoading(true);
-      const response = await apiRequest<{ status: boolean; msg: string; data: { bank_name: string; card_number: string; iban: string; status: string; reason: string; name_family: string; }[]; }>({
+      const response = await apiRequest<{
+        status: boolean;
+        data: {
+          bank_name: string;
+          card_number: string;
+          iban: string;
+          status: string;
+          reason: string;
+          name_family: string;
+        }[];
+      }>({
         url: "/account/credit-card/list",
         method: "GET",
       });
@@ -31,14 +49,14 @@ function BankCardManager() {
           number: item.card_number,
           holder: item.name_family,
           bankName: item.bank_name,
-          status: item.status === "confirm" ? "confirm" : item.status === "pending" ? "pending" : "rejected",
+          status:
+            item.status === "confirm"
+              ? "confirm"
+              : item.status === "pending"
+              ? "pending"
+              : "rejected",
         }));
         setCards(mappedCards);
-
-        if (mappedCards.length === 0) {
-  navigate(ROUTES.BANK_CARDS_EMPTY);
-}
-
       } else {
         setError("دریافت کارت‌ها ناموفق بود");
       }
@@ -58,11 +76,17 @@ function BankCardManager() {
     if (result) fetchCards();
   };
 
+  if (!loading && cards.length === 0) {
+  return <BankCardsPage />;
+}
+
   return (
     <HeaderLayout>
       <div dir="rtl" className="bg-backgroundMain min-h-[400px] w-full">
         <div className="container-style grid grid-col2 gap-8 lg:gap-12">
-          <div className="mt-7 lg:mt-4"><BreadcrumbNavigation /></div>
+          <div className="mt-7 lg:mt-4">
+            <BreadcrumbNavigation />
+          </div>
 
           <div className="flex flex-col lg:flex-row gap-4 lg:gap-12 pb-8">
             <div className="w-full lg:w-2/5 px-4 lg:px-0">
@@ -79,7 +103,10 @@ function BankCardManager() {
               ) : error ? (
                 <p className="text-red-500 animate-fade-in">{error}</p>
               ) : (
-                <BankCardList cards={cards} onAddCard={() => console.log("افزودن کارت")} />
+                <BankCardList
+                  cards={cards}
+                  onAddCard={() => console.log("افزودن کارت")}
+                />
               )}
             </div>
           </div>
